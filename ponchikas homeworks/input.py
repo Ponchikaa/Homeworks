34c7ee5501cd759614c6c49cf757e1755a4a548b
Ponchikaa <lazzexa@gmail.com>
2022-10-04 20:40:36 +0400

    second commit

---
+++ ponchikas homeworks/input.py
@@ -0,0 +1,10 @@
+username = input("write here your name: ")
+surname = input("write here your surname:")
+age = input("write here your age:")
+
+information = "name - {}, surname - {}, age - {}".format(username,surname,age)
+
+
+
+
+print(information)
